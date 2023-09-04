0520ce65022c0fe7fb56a3550c0755e9ccfcd816
Sharif Ahmad <sharifrah@gmail.com>
2023-09-04 16:07:43 +0200

    leetcode 215 Kth largest number - quick sort solution

---
+++ leetcode/215/215-quick-sort.go
@@ -0,0 +1,50 @@
+package main
+
+// Time complexity: O(n * log n)
+// Space complexity: O(n)
+
+func findKthLargest(nums []int, k int) int {
+  n := len(nums)
+
+  return find_recursive(nums, 0, n-1, n-k)
+}
+
+// quick sort technique
+// t: target index
+func find_recursive(nums []int, l, r, t int) int {
+  p := partition(nums, l, r)
+
+  if p > t {
+    return find_recursive(nums, l, p-1, t)
+  } else if p < t {
+    return find_recursive(nums, p+1, r, t)
+  }
+
+  return nums[p]
+}
+
+// Lomuto partition algorithm
+// nums: the number array
+// l: the left index
+// r: right index
+// returns the new (correct) index for the pivot element
+func partition(nums []int, l, r int) int {
+  p := nums[r] // the pivot
+  i, j := l-1, l
+  // main loop
+  for ; j <= r-1; j++ {
+    if nums[j] <= p {
+      i++
+      swap(&nums[i], &nums[j])
+    }
+  }
+
+  i++
+  swap(&nums[i], &nums[r])
+
+  return i
+}
+
+func swap[T any](a, b *T) {
+  *a, *b = *b, *a
+}
---
+++ leetcode/215/215_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "github.com/stretchr/testify/assert"
+  "testing"
+)
+
+func TestFindKthLargest(t *testing.T) {
+
+  assert.Equal(t, 5, findKthLargest([]int{3, 2, 1, 5, 6, 4}, 2))
+  assert.Equal(t, 4, findKthLargest([]int{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4))
+  assert.Equal(t, 2, findKthLargest([]int{1, 1, 1, 2, 3, 4, 4}, 4))
+  assert.Equal(t, 6, findKthLargest([]int{7, 6, 5, 4, 3, 2, 1}, 2))
+  assert.Equal(t, 7, findKthLargest([]int{7, 7}, 2))
+  assert.Equal(t, 5, findKthLargest([]int{5}, 1))
+  assert.Equal(t, 3, findKthLargest([]int{7, 6, 5, 4, 3, 2, 1}, 5))
+}
+
+func TestPartition(t *testing.T) {
+  assert.Equal(t, 0, partition([]int{7, 6, 5, 4, 3, 2, 1}, 0, 6))
+}
+
+func TestSwap_normal_variable(t *testing.T) {
+  var a = 5
+  var b = 10
+
+  swap(&a, &b)
+
+  assert.Equal(t, 10, a)
+  assert.Equal(t, 5, b)
+}
+
+func TestSwap_array(t *testing.T) {
+  A := []int{1, 10}
+
+  swap(&A[0], &A[1])
+
+  assert.Equal(t, []int{10, 1}, A)
+}
