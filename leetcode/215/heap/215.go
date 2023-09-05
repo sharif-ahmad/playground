b8c01fc998b9a39a590d264517d78b4175793bef
Sharif Ahmad <sharifrah@gmail.com>
2023-09-05 16:02:58 +0200

    [leetcode/215] added heap solution

--- leetcode/215/heap/215.go
+++ leetcode/215/heap/215.go
@@ -1,1 +1,149 @@
 package heap
+
+import (
+	"cmp"
+	"errors"
+)
+
+// Could not submit this in leetcode because it does not support go 1.21 yet. :(
+// cmp.Ordering is 1.21 feature
+
+// Time complexity: O(n * log k)
+// Space complexity: O(n + k)
+
+func findKthLargest(nums []int, k int) int {
+	minHeap := NewHeap[int](k, &LTCompareFn[int]{})
+
+	for _, x := range nums {
+		if minHeap.size < k {
+			minHeap.Insert(x)
+		} else if x > minHeap.Top() {
+			minHeap.ExtractTop()
+			minHeap.Insert(x)
+		}
+	}
+
+	return minHeap.Top()
+}
+
+// auxiliary data structures
+
+type LTCompareFn[T cmp.Ordered] struct{}
+
+func (receiver *LTCompareFn[T]) call(a, b T) bool {
+	return a < b
+}
+
+type CompareFn[T any] interface {
+	call(T, T) bool
+}
+
+type Heap[T any] struct {
+	cap       int
+	size      int
+	arr       []T
+	compareFn CompareFn[T]
+}
+
+func NewHeap[T any](cap int, compareFn CompareFn[T]) *Heap[T] {
+	return &Heap[T]{
+		cap:       cap,
+		size:      0,
+		arr:       make([]T, cap),
+		compareFn: compareFn,
+	}
+}
+
+func NewHeapFrom[T any](arr []T, compareFn CompareFn[T]) *Heap[T] {
+	size := len(arr)
+
+	h := &Heap[T]{
+		cap:       size,
+		size:      size,
+		arr:       arr,
+		compareFn: compareFn,
+	}
+
+	for i := size/2 - 1; i >= 0; i-- {
+		h.heapify(i)
+	}
+
+	return h
+}
+
+func (h *Heap[T]) Size() int {
+	return h.size
+}
+
+func (h *Heap[T]) parent(c int) int {
+	return (c - 1) / 2
+}
+
+func (h *Heap[T]) leftChild(p int) int {
+	return 2*p + 1
+}
+
+func (h *Heap[T]) rightChild(p int) int {
+	return 2*p + 2
+}
+
+func (h *Heap[T]) heapify(i int) {
+	newParent := i
+
+	l := h.leftChild(i)
+	if l < h.size && h.compareFn.call(h.arr[l], h.arr[newParent]) {
+		newParent = l
+	}
+
+	r := h.rightChild(i)
+	if r < h.size && h.compareFn.call(h.arr[r], h.arr[newParent]) {
+		newParent = r
+	}
+
+	if newParent == i {
+		return
+	}
+
+	swap(&h.arr[newParent], &h.arr[i])
+	h.heapify(newParent)
+}
+
+func (h *Heap[T]) Insert(x T) error {
+	if h.size == h.cap {
+		return errors.New("capacity full")
+	}
+
+	i := h.size
+	h.size++
+
+	h.arr[i] = x
+
+	for i > 0 {
+		p := h.parent(i)
+		if !h.compareFn.call(h.arr[i], h.arr[p]) {
+			break
+		}
+		swap(&h.arr[i], &h.arr[p])
+
+		i = p
+	}
+
+	return nil
+}
+
+func (h *Heap[T]) Top() T {
+	return h.arr[0]
+}
+
+func (h *Heap[T]) ExtractTop() T {
+	swap(&h.arr[0], &h.arr[h.size-1])
+	h.size--
+
+	h.heapify(0)
+
+	return h.arr[h.size]
+}
+
+func swap[T any](a, b *T) {
+	*a, *b = *b, *a
+}
--- leetcode/215/heap/215_test.go
+++ leetcode/215/heap/215_test.go
@@ -1,1 +1,95 @@
 package heap
+
+import (
+	"cmp"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// auxiliary method for testing
+func (h *Heap[T]) getArr() []T {
+	return h.arr[0:h.size]
+}
+
+type GTCompareFn[T cmp.Ordered] struct{}
+
+func (c *GTCompareFn[T]) call(a, b T) bool {
+	return a > b
+}
+
+func TestFindKthLargest(t *testing.T) {
+
+	assert.Equal(t, 5, findKthLargest([]int{3, 2, 1, 5, 6, 4}, 2))
+	assert.Equal(t, 4, findKthLargest([]int{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4))
+	assert.Equal(t, 2, findKthLargest([]int{1, 1, 1, 2, 3, 4, 4}, 4))
+	assert.Equal(t, 6, findKthLargest([]int{7, 6, 5, 4, 3, 2, 1}, 2))
+	assert.Equal(t, 7, findKthLargest([]int{7, 7}, 2))
+	assert.Equal(t, 5, findKthLargest([]int{5}, 1))
+	assert.Equal(t, 3, findKthLargest([]int{7, 6, 5, 4, 3, 2, 1}, 5))
+}
+
+func TestNewHeapFrom(t *testing.T) {
+	A := []int{1, 2, 4, 5, 5, 6}
+	h := NewHeapFrom(A, &GTCompareFn[int]{})
+
+	assert.Equal(t, 6, h.Top())
+	assert.Equal(t, []int{6, 5, 4, 5, 2, 1}, h.getArr())
+}
+
+func TestMaxHeap_Insert(t *testing.T) {
+	t.Run(`when heap is built with NewMaxHeap`, func(t *testing.T) {
+
+		t.Run(`when heap is empty`, func(t *testing.T) {
+			h := NewHeap[int](2, &GTCompareFn[int]{})
+			err := h.Insert(5)
+
+			assert.Nil(t, err)
+			assert.Equal(t, []int{5}, h.getArr())
+		})
+
+		t.Run(`when heap is not empty`, func(t *testing.T) {
+			h := NewHeap[int](2, &GTCompareFn[int]{})
+			h.Insert(5)
+
+			err := h.Insert(7)
+
+			assert.Nil(t, err)
+			assert.Equal(t, []int{7, 5}, h.getArr())
+		})
+
+		t.Run(`when heap is full`, func(t *testing.T) {
+			h := NewHeap[int](2, &GTCompareFn[int]{})
+			h.Insert(5)
+			h.Insert(7)
+
+			err := h.Insert(3)
+
+			assert.NotNil(t, err)
+		})
+
+		t.Run(`example`, func(t *testing.T) {
+			h := NewHeap[int](4, &LTCompareFn[int]{})
+			h.Insert(3)
+			h.Insert(3)
+			h.Insert(4)
+			h.Insert(5)
+			h.ExtractTop()
+			h.Insert(5)
+
+			assert.Equal(t, 3, h.Top())
+		})
+
+	})
+
+	//t.Run(`when heap is built with NewMaxHeapFrom`, func(t *testing.T) {
+	//
+	//})
+}
+
+func TestHeap_ExtractTop(t *testing.T) {
+	t.Run(`When heap is full`, func(t *testing.T) {
+		h := NewHeapFrom([]int{1, 1, 1, 2}, &LTCompareFn[int]{})
+
+		assert.Equal(t, 1, h.ExtractTop())
+	})
+}
